2861dfeac46c4dcdd83d071a9a6b7e505b380d2a
Rickard Pedersen <pedersen.rickard@outlook.com>
2020-09-17 11:14:18 +0200

    test

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,6 +3,7 @@ const userModel = require('../models/user')
 module.exports = {
     authenticateUser: async (req, res) => {
         try {
+            return res.status(200)
             const userObject = await userModel.authenticateUser(req.body.username, req.body.password)
 
             if (userObject.success) {
